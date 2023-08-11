4b63adf0a27af08ccdfa19b6f41803f82efabba3
Krisna Juan Fernando <krisnajufer07@gmail.com>
2023-08-11 19:47:29 +0700

    selesai membuat api auth

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
 
-    public function validatorHelper($request)
+    public function validatorLoginHelper($request)
     {
         App::setLocale("id");
         $rules = [
@@ -20,13 +22,38 @@ class AuthController extends Controller
 
         $validator = Validator::make($request, $rules);
 
-        return $validator->getMessageBag();
+        return $validator;
     }
 
     public function login(Request $request)
     {
-        $validator = $this->validatorHelper($request->all());
-        $data = ['message' => $validator];
+        $validator = $this->validatorLoginHelper($request->all());
+        if ($validator->fails()) {
+            $data = ['message' => $validator->getMessageBag()];
+            return response()->json($data, 400);
+        }
+
+        if (!Auth::attempt($request->all())) {
+            return response()->json(['message' => 'Unathorized'], 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $data = [
+            'user' => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ];
+
         return response()->json($data, 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(["message" => 'Logged Out'], 200);
     }
 }
